fca3691fe74468a7d86a2d0946b87ee9259cc3fd
Maxim Vialyx <maksimvialyx@gmail.com>
2018-11-13 00:30:06 +0300

    - change repository

--- JsonCoddableUrlSession/JsonCoddableUrlSession/Repository.swift
+++ JsonCoddableUrlSession/JsonCoddableUrlSession/Repository.swift
@@ -15,27 +15,39 @@ struct FlightData: Codable {
     let ActualTime: String
 }
 
+enum RepositoryType {
+    case local, remote
+}
+
 final class Repository {
     
     private let apiClient: APIClient!
+    private let database: Database!
     
-    init(apiClient: APIClient) {
+    init(apiClient: APIClient, database: Database) {
         self.apiClient = apiClient
+        self.database = database
     }
     
-    func getFlights(_ completion: @escaping ((Result<[FlightData]>) -> Void)) {
-        let resource = Resource(url: URL(string: "https://desolate-beyond-86929.herokuapp.com/arrival")!)
-        apiClient.load(resource) { (result) in
-            switch result {
-            case .success(let data):
-                do {
-                    let items = try JSONDecoder().decode([FlightData].self, from: data)
-                    completion(.success(items))
-                } catch {
+    func getFlights(_ type: RepositoryType, _ completion: @escaping ((Result<[FlightData]>) -> Void)) {
+        switch type {
+        case .local:
+            completion(.success(database.get()))
+        case .remote:
+            let resource = Resource(url: URL(string: "https://desolate-beyond-86929.herokuapp.com/arrival")!)
+            apiClient.load(resource) { [weak self] (result) in
+                switch result {
+                case .success(let data):
+                    do {
+                        let items = try JSONDecoder().decode([FlightData].self, from: data)
+                        self?.database.save(flights: items)
+                        completion(.success(items))
+                    } catch {
+                        completion(.failure(error))
+                    }
+                case .failure(let error):
                     completion(.failure(error))
                 }
-            case .failure(let error):
-                completion(.failure(error))
             }
         }
     }
